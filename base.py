ac52f3806d8e4e360bb8c1abfbceedee28dde9cb
Taha Elsenosy <tahaelsenosy@gmail.com>
2022-08-18 17:55:28 +0200

    Initial commit

---
+++ base.py
@@ -0,0 +1,42 @@
+#!/env/python
+
+from flask import Flask
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By;
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from sys import argv
+from markupsafe import escape
+
+
+# define chrome driver path
+DRIVER_PATH = '../chromedriver'
+
+app = Flask(__name__)
+@app.route("/deltas/<id>")
+def hello_world(id):
+    deltaId = escape(id)
+    try:
+        driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+
+        driver.get('https://deri.io/#/trade/options/'+deltaId)
+
+        # sleep until page fully loades
+        sleep(1)
+
+        # Get contact items
+        items = driver.find_element(By.CLASS_NAME, 'contract')
+        
+        # Get c-row
+        orders = items.find_elements(By.CLASS_NAME, 'c-row')
+
+        # Delta object
+        print(orders[10].text.split('\n'))
+        return orders[10].text.split('\n')
+        
+    finally:
+        driver.quit()
+
